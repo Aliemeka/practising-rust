7eea32ab997f13dce050b2cb682fc9a8f7922e3c
Emeka Allison <emekaallison4@gmail.com>
2023-03-27 14:13:04 +0100

    Feat: Odd and even sums

--- calculate-algo/src/main.rs
+++ calculate-algo/src/main.rs
@@ -1,8 +1,15 @@
-fn calculate(bottom: i32, top: i32) -> i32 {
+fn sum_evens(bottom: i32, top: i32) -> i32 {
     (bottom..=top).filter(|n| n % 2 == 0).sum()
 }
 
+fn sum_odds(bottom: i32, top: i32) -> i32 {
+    (bottom..=top).filter(|n| n % 2 != 0).sum()
+}
 fn main() {
-    let range_sum = calculate(2, 20);
-    println!("The range sum of even numbers from 2 to 20 = {range_sum}");
+    let bottom = 2;
+    let top = 20;
+    let even_sum = sum_evens(bottom, top);
+    let odd_sum = sum_odds(bottom, top);
+    println!("The range sum of even numbers from 2 to 20 = {even_sum}");
+    println!("The range sum of even numbers from 2 to 20 = {odd_sum}");
 }
